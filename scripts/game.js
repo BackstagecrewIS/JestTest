d6363280907890d9df54668fd1d7747f5a320a63
BackstagecrewIS <locksmith@backstagecrew.com>
2022-11-27 19:02:54 +0000

    initial commit

---
+++ scripts/game.js
@@ -0,0 +1,18 @@
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"],
+};
+
+function newGame() {
+    game.score = 0;
+    game.playerMoves = [];
+    game.currentGame = [];
+}
+
+
+
+
+
+module.exports = { game, newGame };
